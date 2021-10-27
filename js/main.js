8e3a2af612042d11923f6f2ca7119ee738782224
Михаил Чернышов <makvorn@yandex.ru>
2021-10-27 23:44:07 +0300

    7.10. Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
 /* eslint-disable no-unused-vars */
-import {arrayGuests} from './data-object.js';
+import {arrayGuests} from './utils/data-object.js'; //консоль.лог опять перестал работать, посмотрите пожалуйста код что не так, я вроде ничего не менял
+import {similarListFragment} from './utils/similar-ads.js'; //В задании сказано подключить модуль в проект, это сюда или в index.html
+
